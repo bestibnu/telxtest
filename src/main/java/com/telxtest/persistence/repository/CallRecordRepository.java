09bbfdccb2b21fe3badb2621365b4aedea37db22
besti <bestibnu@gmail.com>
2026-02-10 20:23:42 +0530

    Add call event handling and credit charging

--- src/main/java/com/telxtest/persistence/repository/CallRecordRepository.java
+++ src/main/java/com/telxtest/persistence/repository/CallRecordRepository.java
@@ -2,8 +2,10 @@ package com.telxtest.persistence.repository;
 
 import com.telxtest.persistence.entity.CallRecordEntity;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CallRecordRepository extends JpaRepository<CallRecordEntity, Long> {
     List<CallRecordEntity> findAllByOrderByStartedAtDesc();
+    Optional<CallRecordEntity> findByCallSid(String callSid);
 }
